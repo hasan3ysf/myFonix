9618ae35746365ab20e78f630f1388537d2f9973
“Hasan <hasan.ajsf@gmail.com>
2016-05-19 23:12:49 +0300

    Clean Master Branch

--- client/chrome/main.js
+++
@@ -1,3 +0,0 @@
-/* This can be used as main function instead of (function main(){ })(); */
-//document.addEventListener("DOMContentLoaded", startSpeach);
-// document.addEventListener('DOMContentLoaded', function () { })
